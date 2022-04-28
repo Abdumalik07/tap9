61f7e9a7d862efc2e7843378d932cc52e8ea6dec
Abdumalik07 <104436538+Abdumalik07@users.noreply.github.com>
2022-04-28 11:37:44 +0600

    Initial commit

---
+++ src/com/company/Tapshyrma9.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Random;
+
+public class Tapshyrma9 {
+    public static void main(String[] args) {
+
+    }
+    static void tap9(int[] arr){
+        System.out.println(arr);
+    }
+}
